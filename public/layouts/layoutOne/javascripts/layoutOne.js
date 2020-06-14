60ec72f20a40b5fab1511e17bc24966e4a73b73f
Jorge aml <martjorge8@gmail.com>
2020-06-13 18:59:52 -0600

    Update layoutOne.js

--- public/layouts/layoutOne/javascripts/layoutOne.js
+++ public/layouts/layoutOne/javascripts/layoutOne.js
@@ -2,10 +2,11 @@ $(document).ready( function () {
     var a = document.getElementsByClassName('nav');
     /*INSERT YOUR CLASSES HERE*/
     var classes = ['navbar', 'navbar_items', 'navbar_link'];
-    var linkClasses = ['link1'];
+    var linkClasses = ['link1', 'material-icons', 'md-18', 'md-light'];
     var urlLinks = ['#', '#', '#', '#', '#'];
     var text = ['Coder Dojo Guatemala', 'Proyectos', 'Tutoriales', 'Blog', 'Donaciones'];
 
+    /*links their classes and references*/
     var aTag = $("<a>{{t0}}</a>").attr({
         href: urlLinks[0],
         class: linkClasses[0]
@@ -14,6 +15,25 @@ $(document).ready( function () {
         href: urlLinks[1],
         class: linkClasses[0]
     });
+    var cTag = $("<a>{{t2}}</a>").attr({
+        href: urlLinks[2],  
+        class: linkClasses[0]
+    });
+    var dTag = $("<a>{{t3}}</a>").attr({
+        href: urlLinks[3],
+        class: linkClasses[0]
+    });
+    var eTag = $("<a>{{t4}}</a>").attr({
+        href: urlLinks[4],
+        class: linkClasses[0]
+    });
+    for(var i = 1; i <= linkClasses.length; i++){
+        var ff = linkClasses[i];
+    };
+    var fTag = $("<i>more_vert</i>").attr({
+        class: "material-icons md-18 md-light"
+    });
+    
 
     
     /*MANIPULATION BEGIN*/
@@ -25,6 +45,11 @@ $(document).ready( function () {
     $(".navbar").find("nav").eq(0).addClass(classes[1]);
     $(".navbar").find("div").eq(0).addClass(classes[2]).html(aTag);
     $(".navbar").find("div").eq(2).addClass(classes[2]).html(bTag);
+        //update
+    $(".navbar").find("div").eq(3).addClass(classes[2]).html(cTag);
+    $(".navbar").find("div").eq(4).addClass(classes[2]).html(dTag);
+    $(".navbar").find("div").eq(5).addClass(classes[2]).html(eTag);
+    $(".navbar").find("div").eq(6).addClass(classes[2]).html(fTag);
         //textA.appendTo(aTag);
     //space
     new Vue({
@@ -32,6 +57,9 @@ $(document).ready( function () {
         data: {
             t0: text[0],
             t1: text[1],
+            t2: text[2],
+            t3: text[3],
+            t4: text[4],
             k0: linkClasses[0]
         }
     });

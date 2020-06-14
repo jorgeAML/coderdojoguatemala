a30549173a13e2f485bda32105429c10056e82ae
Jorge aml <martjorge8@gmail.com>
2020-06-13 18:03:12 -0600

    Create layoutOne.js

---
+++ public/layouts/layoutOne/javascripts/layoutOne.js
@@ -0,0 +1,33 @@
+$(document).ready( function () {
+    var a = document.getElementsByClassName('nav');
+    /*INSERT YOUR CLASSES HERE*/
+    var classes = ['navbar', 'navbar_items', 'navbar_link'];
+    var linkClasses = ['link1'];
+    var urlLinks = ['#', '#', '#', '#', '#'];
+    var text = ['Coder Dojo Guatemala', 'Proyectos', 'Tutoriales', 'Blog', 'Donaciones'];
+
+    var aTag = $("<a>{{t0}}</a>").attr({
+        href: urlLinks[0],
+        class: linkClasses[0]
+    });
+
+    
+    /*MANIPULATION BEGIN*/
+    $("<div/>",{
+        "appendTo": a,
+        "class": classes[0],
+        html: "<div/><div/><nav><div/><div/><div/><div/><div/>"
+    });
+    $(".navbar").find("nav").eq(0).addClass(classes[1]);
+    $(".navbar").find("div").eq(0).addClass(classes[2]).html(aTag);
+    $(".navbar").find("div").eq(2).addClass(classes[2]).html()
+        //textA.appendTo(aTag);
+    //space
+    new Vue({
+        el: ".nav",
+        data: {
+            t0: text[0],
+            k0: linkClasses[0]
+        }
+    });
+});

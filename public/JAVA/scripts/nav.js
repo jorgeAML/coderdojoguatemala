c38133e52c84aa881813ae79f1717dd7185dc784
JorgeAML <martjorge8@gmail.com>
2020-12-24 23:05:17 -0600

    nav updated only for desktops in Java Sections

--- public/JAVA/scripts/nav.js
+++ public/JAVA/scripts/nav.js
@@ -1,31 +1,36 @@
 /**HERE WILL BE ALL SCRIPTS FOR THE NAV IN JAVA */
 $(document).ready(function() {
+    var links = [
+        { ref0: '#'}
+    ];
+
     $("<div/>", {
         "appendTo": "#nav-java",
         "class": "navbar",
         "html": "<div/><div/><div/><div/><div/><div/>"
     });
-    $('.navbar').find('div').eq(0).html('{{title1}}').attr({
+    $('.navbar').find('div').eq(0).html("<a class='links-design' href='#'>{{title1}}</a>").attr({
         "class": "sub-btn"
     });
-    $('.navbar').find('div').eq(1).html("{{title2}}").attr({
+    $('.navbar').find('div').eq(1).html("<a href='#'>{{title2}}</a>").attr({
         "class": "sub-btn"
     });
-    $('.navbar').find('div').eq(2).html("{{title3}}").attr({
+    $('.navbar').find('div').eq(2).html("<a href='#'>{{title3}}</a>").attr({
         "class": "sub-btn"
     });
-    $('.navbar').find('div').eq(3).html("{{title4}}").attr({
+    $('.navbar').find('div').eq(3).html("<a href='#'>{{title4}}</a>").attr({
         "class": "sub-btn"
     });
-    $('.navbar').find('div').eq(4).html("{{title5}}").attr({
+    $('.navbar').find('div').eq(4).html("<a href='#'>{{title5}}</a>").attr({
         "class": "sub-btn"
     });
-    $('.navbar').find('div').eq(5).html("{{title6}}").attr({
+    $('.navbar').find('div').eq(5).html("<a>{{title6}}</a>").attr({
         "class": "sub-btn"
     });
     /*VARIABLES FOR VUE*/
-    const TITLES = ["Coder Dojo Guatemala", "Reglas", "Sobre el Dojo", "Acerca de", "Donar", "ToggleButton"];
-    var yourFear = ["subDiv"];
+    var TITLES = ["Coder Dojo Guatemala", "Reglas", "Sobre el Dojo", "Acerca de", "Donar", "ToggleButton"];
+    
+    var YOURFEAR = ["links-design"];
     /**VUE IN ACTIONS */
     new Vue ({
         el: "#nav-java",
@@ -36,7 +41,7 @@ $(document).ready(function() {
             title4: TITLES[3],
             title5: TITLES[4],
             title6: TITLES[5],
-            cls1: yourFear[0]
+            YF0: YOURFEAR[0],
         }
     });
 

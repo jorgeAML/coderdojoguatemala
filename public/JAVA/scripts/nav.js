878a879c395aa3e98e9f5fdb93b0393fbfe27126
JorgeAML <martjorge8@gmail.com>
2020-12-24 22:11:50 -0600

    adding features for Java Sections

--- public/JAVA/scripts/nav.js
+++ public/JAVA/scripts/nav.js
@@ -5,14 +5,27 @@ $(document).ready(function() {
         "class": "navbar",
         "html": "<div/><div/><div/><div/><div/><div/>"
     });
-    $('.navbar').find('div').eq(0).html('{{title1}}');
-    $('.navbar').find('div').eq(1).html("{{title2}}");
-    $('.navbar').find('div').eq(2).html("{{title3}}");
-    $('.navbar').find('div').eq(3).html("{{title4}}");
-    $('.navbar').find('div').eq(4).html("{{title5}}");
-    $('.navbar').find('div').eq(4).html("{{title6}}");
+    $('.navbar').find('div').eq(0).html('{{title1}}').attr({
+        "class": "sub-btn"
+    });
+    $('.navbar').find('div').eq(1).html("{{title2}}").attr({
+        "class": "sub-btn"
+    });
+    $('.navbar').find('div').eq(2).html("{{title3}}").attr({
+        "class": "sub-btn"
+    });
+    $('.navbar').find('div').eq(3).html("{{title4}}").attr({
+        "class": "sub-btn"
+    });
+    $('.navbar').find('div').eq(4).html("{{title5}}").attr({
+        "class": "sub-btn"
+    });
+    $('.navbar').find('div').eq(5).html("{{title6}}").attr({
+        "class": "sub-btn"
+    });
     /*VARIABLES FOR VUE*/
     const TITLES = ["Coder Dojo Guatemala", "Reglas", "Sobre el Dojo", "Acerca de", "Donar", "ToggleButton"];
+    var yourFear = ["subDiv"];
     /**VUE IN ACTIONS */
     new Vue ({
         el: "#nav-java",
@@ -22,7 +35,8 @@ $(document).ready(function() {
             title3: TITLES[2],
             title4: TITLES[3],
             title5: TITLES[4],
-            title6: TITLES[5]
+            title6: TITLES[5],
+            cls1: yourFear[0]
         }
     });
 

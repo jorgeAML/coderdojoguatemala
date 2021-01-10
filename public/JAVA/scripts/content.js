d323e80e788cbff8743213051e89c38af8ea3460
JorgeAML <martjorge8@gmail.com>
2021-01-09 23:02:25 -0600

    New update design for Java section

--- public/JAVA/scripts/content.js
+++ public/JAVA/scripts/content.js
@@ -1,25 +1,60 @@
 $(document).ready(function() {
     const TITLE = "JAVA";
-    var infoForJava = "This is a test, after will be more information dddddd";
     $("<div/>", {
         "appendTo": ".divide-center",
         "class": "container-divided",
-        "html": "<div/><div/>"
+        html: "<div/><div/>"
     });
-    $(".container-divided").find("div").eq(0).html("cd1").attr({
+    $(".container-divided").find("div").eq(0).html(" ").attr({
         "class": "cd1"
     });
-    $(".container-divided").find("div").eq(1).html("cd2").attr({
+    $(".container-divided").find("div").eq(1).html(" ").attr({
         "class": "cd2"
     });
     //APPENDT TO CD1 FOR INTRODUCTION
     $("<div/>", {
         "appendTo": ".cd1",
         "class": "info-java",
-        "html": "<div/>"
+        html: "<div/>"
     });
     //INSIDE OF THIS ONE, WILL BE ALL THE INFORMATION
-    $(".info-java").find("div").eq(0).html(TITLE + "\n" + infoForJava).attr({
+    $(".info-java").find("div").eq(0).attr({
         "class": "design-info-java"
-    })
+    });
+    $("<h1>", { "appendTo": ".design-info-java", "class": "title", html: TITLE})
+    //CHAPTERS
+    $("<div/>", {
+        "appendTo": ".cd2",
+        "class": "chapters-java",
+        html: "<div/><div/>"
+    });
+    $(".chapters-java").find("div").eq(0).attr({
+        "class": "chap1 id99"
+    });
+    $("<img>", { "appendTo": ".id99", src: "images/flame-delete-confirmation.png"});
+    $("<h3/>", { "appendTo": ".id99", html: "{{CP1}}" });
+    $("<p/>", { "appendTo": ".id99", html: "{{CPT1}}" });
+    $(".chapters-java").find("div").eq(1).attr({ 
+        "class": "chap1 id98"
+    });
+    $("<img>", { "appendTo": ".id98", src: "images/flame-space-adventures.png"});
+    $("<h3/>", { "appendTo": ".id98", html: "{{CP2}}" });
+    $("<p/>", { "appendTo": ".id98", html: "{{CPT2}}" });
+    //VARIABLES
+    var imageTest1 = ["images/flame-delete-confirmation.png", "images/flame-space-adventures.png"];
+    var chapter1 = ["Introduccion", "Aprende a crear programas utilizando el lenguaje de programacion JAVA, el poder de la mente es increible."];
+    var chapter2 = ["Programacion Elemental", "Para poder entender temas complejos una buena base sobre que es la programacion sera de bastante ayuda."];
+    new Vue({
+        el: ".chapters-java",
+        data: {
+            IT1: imageTest1[0],
+            IT2: imageTest1[1],
+            CP1: chapter1[0],
+            CPT1: chapter1[1],
+            CP2: chapter2[0],
+            CPT2: chapter2[1]
+        }
+    });
+    console.log(chapter1[1].length);
+    console.log(chapter2[1].length);
 });
